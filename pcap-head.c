5f07cd03d6e99e8abecdb7970880207a7ac0d52b
Jakob Kramer <jakob.kramer@gmx.de>
2016-06-08 14:53:19 +0200

    Switch from Unlicense to CC0

--- pcap-head.c
+++ pcap-head.c
@@ -1,3 +1,18 @@
+/*
+ * pcap-head - truncate packet captures
+ *
+ * Written in 2015 by Jakob Kramer <jakob.kramer@gmx.de>
+ *
+ * To the extent possible under law, the author(s) have dedicated all
+ * copyright and related and neighboring rights to this software to the
+ * public domain worldwide. This software is distributed without any
+ * warranty.
+ *
+ * You should have received a copy of the CC0 Public Domain Dedication
+ * along with this software.
+ * If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+
 #include <arpa/inet.h>
 #include <stdint.h>
 #include <stdio.h>
